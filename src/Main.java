c4aa290b206930da8f99cbe2ffee0ae8636c394c
K. Max <113927586+KMaxKR@users.noreply.github.com>
2023-02-20 20:31:58 +0200

    Interface

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,18 @@ import run.Programator;
 
 public class Main {
     public static void main(String[] args) {
-        Shape shape = new Shape();
-        System.out.println(shape.getArea());
+        //Shape shape = new Shape();
+        //System.out.println(shape.getArea());
 
-
-        //
 
         Programator prog = new Programator();
+        prog.setNume("K. Max");
+        prog.setAge(18);
+        prog.getDescription();
         prog.lucreaza();
+        prog.runner();
+
+        System.out.println("Name is " + prog.getNume());
+        System.out.println("Age is " + prog.getAge());
     }
 }
