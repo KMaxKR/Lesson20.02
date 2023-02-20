1440c2253e5a56570ca6b6058bc34fa98ae4bf66
K. Max <113927586+KMaxKR@users.noreply.github.com>
2023-02-20 20:28:33 +0200

    Interface

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 import Shape.Shape;
+import run.AngajatCompanieTelecomunicatie;
+import run.Programator;
 
 public class Main {
     public static void main(String[] args) {
         Shape shape = new Shape();
         System.out.println(shape.getArea());
+
+
+        //
+
+        Programator prog = new Programator();
+        prog.lucreaza();
     }
 }
