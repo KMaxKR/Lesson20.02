fff62a1b6334ae509a45a8636f1b29f76610b1a3
K. Max <113927586+KMaxKR@users.noreply.github.com>
2023-02-20 20:16:25 +0200

    Interface

---
+++ src/run/AngajatCompanieTelecomunicatie.java
@@ -0,0 +1,31 @@
+package run;
+
+public class AngajatCompanieTelecomunicatie {
+    String nume;
+    int age;
+    String description;
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/run/BuilderCompanyWorker.java
@@ -0,0 +1,8 @@
+package run;
+
+public class BuilderCompanyWorker extends AngajatCompanieTelecomunicatie implements Angajat, Runner {
+    @Override
+    public void lucreaza() {
+
+    }
+}
